835c0f8c500aca733d2ac3a01c92a6922bf11596
Dongbin <bnitech88@gmail.com>
2022-05-30 21:03:56 +0900

    [dongbin] #3.2.3

--- src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/controller/post/PostApiController.kt
+++ src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/controller/post/PostApiController.kt
@@ -1,21 +1,25 @@
 package com.bnitech.springbootwebmvcdevelopedbykotlin.controller.post
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import com.bnitech.springbootwebmvcdevelopedbykotlin.model.http.UserRequest
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
 class PostApiController {
 
     @PostMapping("/post-mapping")
-    fun post(): String {
+    fun postMapping(): String {
         return "post-mapping"
     }
 
     @RequestMapping(method = [RequestMethod.POST], path = ["/request-mapping"])
     fun requestMapping(): String {
         return "request-mapping"
+    }
+
+    @PostMapping("/post-mapping/object")
+    fun postMappingObject(@RequestBody userRequest: UserRequest): UserRequest {
+        println("userRequest = [${userRequest}]")
+        return userRequest
     }
 }
