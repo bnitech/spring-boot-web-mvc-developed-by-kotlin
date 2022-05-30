8305c4f320cfa98066a7169eb03548731293980b
Dongbin <bnitech88@gmail.com>
2022-05-30 21:08:25 +0900

    [dongbin] #3.2.4

--- src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/model/http/UserRequest.kt
+++ src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/model/http/UserRequest.kt
@@ -1,8 +1,13 @@
 package com.bnitech.springbootwebmvcdevelopedbykotlin.model.http
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class UserRequest(
     var name: String? = null,
     var age: Int? = null,
     var email: String? = null,
-    var address: String? = null
+    var address: String? = null,
+
+    @JsonProperty("phone_number")
+    var phoneNumber: String? = null
 )
