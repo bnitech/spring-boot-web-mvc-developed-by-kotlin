3802c264ebe8f8909d932e5aa79f58fd0d8e0f11
Dongbin <bnitech88@gmail.com>
2022-05-30 21:10:24 +0900

    [dongbin] #3.2.5

--- src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/model/http/UserRequest.kt
+++ src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/model/http/UserRequest.kt
@@ -1,13 +1,13 @@
 package com.bnitech.springbootwebmvcdevelopedbykotlin.model.http
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequest(
     var name: String? = null,
     var age: Int? = null,
     var email: String? = null,
     var address: String? = null,
-
-    @JsonProperty("phone_number")
     var phoneNumber: String? = null
 )
