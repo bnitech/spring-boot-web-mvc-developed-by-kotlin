8c1600263c5a4a40be66547f28d72f08404d3058
Dongbin <bnitech88@gmail.com>
2022-05-30 19:58:13 +0900

    [dongbin] #3.1.2

--- src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/controller/get/GetApiController.kt
+++ src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/controller/get/GetApiController.kt
@@ -1,15 +1,24 @@
 package com.bnitech.springbootwebmvcdevelopedbykotlin.controller.get
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
 class GetApiController {
 
-    @GetMapping("/hello")
+    @GetMapping(path = ["/hello", "abcd"])
     fun hello(): String {
         return "hello kotlin"
+    }
+
+    @RequestMapping(method = [RequestMethod.GET], path = ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping"
+    }
+
+    @GetMapping("/get-mapping/path-variable/{name}")
+    fun pathVariable(@PathVariable name: String): String {
+        println(name)
+        return name
     }
 }
