6417373886c657a9fa13c825a7b205a3eba80d6f
Dongbin <bnitech88@gmail.com>
2022-05-31 15:21:45 +0900

    [dongbin] #3.5

---
+++ src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/controller/response/ResponseApiController.kt
@@ -0,0 +1,16 @@
+package com.bnitech.springbootwebmvcdevelopedbykotlin.controller.response
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/response")
+class ResponseApiController {
+
+    @GetMapping
+    fun getMapping(@RequestParam age: Int): String {
+        return "OK"
+    }
+}
