a64667544be621d3c55f1b166fce136c26e8217d
Dongbin <bnitech88@gmail.com>
2022-05-31 15:27:29 +0900

    [dongbin] #3.5.2

--- src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/controller/response/ResponseApiController.kt
+++ src/main/kotlin/com/bnitech/springbootwebmvcdevelopedbykotlin/controller/response/ResponseApiController.kt
@@ -1,5 +1,6 @@
 package com.bnitech.springbootwebmvcdevelopedbykotlin.controller.response
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -10,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController
 class ResponseApiController {
 
     @GetMapping
-    fun getMapping(@RequestParam age: Int?): String {
-        println("age = [${age}]")
-        return "OK"
+    fun getMapping(@RequestParam age: Int?): ResponseEntity<String> {
+
+        if(age == null){
+            return ResponseEntity.status(400).body("age 값이 누락되었습니다.")
+        }
+
+        if(age <20){
+            return ResponseEntity.status(400).body("age 값은 20보다 커야합니다.")
+        }
+
+        return ResponseEntity.ok("OK")
     }
 }
